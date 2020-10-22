daa298f2f566e77c70a3b762c72225ec2bb48616
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-10-22 12:34:21 +0100

    Agregar archivos de proyecto.

---
+++ UTF2-EJER7/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+
+namespace UTF2_EJER7
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+
+
+        private void pequeñoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            textoTextBlock.FontSize =Int32.Parse((sender as RadioButton).Tag.ToString());
+        }
+    }
+}
